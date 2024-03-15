79cee14a4e3ac936a3e11bbc2f7f433e0a13911c
Kirill Gusev <myaumuraa@gmail.com>
2024-03-15 18:40:03 +0300

    Init commit

---
+++ src/test/java/TestExample.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class TestExample {
+    @Test
+    public void testExample() {
+
+    }
+}
